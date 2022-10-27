c8d54894075887a02ba3d92041c8884d89fc7167
SUYASH15012001 <sakshams1501@gmail.com>
2022-10-27 19:59:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// // import logo from "./logo.svg";
+// import "./App.css";
+// import React from "react";
+
+// function App() {
+//   const [data, setdata] = React.useState(
+//     localStorage.getItem("items")
+//       ? JSON.parse(localStorage.getItem("items"))
+//       : []
+//   );
+//   const [txt, setText] = React.useState("");
+
+//   const handleSubmit = () => {
+//     if (txt !== "") {
+//       var d = data.concat(txt);
+//       setdata(d);
+//       setText("");
+//       // console.log(d);
+//       localStorage.setItem("items", JSON.stringify(d));
+//     }
+//   };
+//   // console.log("data ", data);
+//   return (
+//     <div className="App">
+//       <input value={txt} onChange={(e) => setText(e.target.value)} />
+//       <button disabled={!txt.length} onClick={handleSubmit}>
+//         Submit
+//       </button>
+//       {data.map((i) => (
+//         <p>{i}</p>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+      <h1>Hello world</h1>
+    </p>
   );
 }
 
